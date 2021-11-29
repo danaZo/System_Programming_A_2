8f7a1b8ce080baeb8a2e57bc56d4f8fdfa9b02fb
Lior Breitman <76751010+LiorBreitman8234@users.noreply.github.com>
2021-11-29 20:48:15 +0200

    updated main

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ int main ()
     int first,second;
     while(choice != 'D')
     {
-        printf("welcome to my_mat menu!\n");
-        printf("to input data into matrix press A\n");
-        printf("to check if there is a path between two nodes press B\n");
-        printf("to search for the shortest path press C\n");
-        printf("to exit the program press D\n");
-        printf("your choice: ");
+        // printf("welcome to my_mat menu!\n");
+        // printf("to input data into matrix press A\n");
+        // printf("to check if there is a path between two nodes press B\n");
+        // printf("to search for the shortest path press C\n");
+        // printf("to exit the program press D\n");
+        // printf("your choice: ");
         scanf("%c",&choice);
 
         switch (choice)
